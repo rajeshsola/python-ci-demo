8c003bd12d42e59e1a77925b8fe0c6f2538feebb
Rajesh Sola <rajeshsola@gmail.com>
2024-09-14 14:12:05 +0530

    Update myfun.py

--- myfun.py
+++ myfun.py
@@ -3,7 +3,6 @@
 
 def is_leap(yy):
     """Functin to check given year is leap or not."""
-    if yy % 400 == 0 or (yy % 4 == 0 and yy % 100 != 0):
-        return True
-    else:
-        return False
+    return yy % 400 == 0 or (yy % 4 == 0 and yy % 100 != 0)
+
+    
